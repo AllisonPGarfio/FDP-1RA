46755b0bdc5bb2089a35aa943ba13d3f51caf842
AllisonPGarfio <allisonpalomag@gmail.com>
2025-08-26 21:18:57 -0600

    Add files via upload

---
+++ EVA_2_DATOS/src/eva_2_datos/EVA_2_DATOS.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva_2_datos;
+
+/**
+ *
+ * @author garfi
+ */
+public class EVA_2_DATOS {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("ALLISON GARFIO");//Cadena de texto
+        System.out.println(1000);
+        System.out.println(56.983);
+        System.out.println(true);//Valores lógicos 
+        System.out.println('A');//Caracter
+        System.out.println("A");//Cadena de texto
+
+    }
+    
+}
