b1bcd219ef109cc00b349b0f48cb0b061e21fc67
AllisonPGarfio <allisonpalomag@gmail.com>
2025-08-27 22:16:05 -0600

    Add files via upload

---
+++ EVA1_5_VARIABLES/src/eva1_5_variables/EVA1_5_VARIABLES.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva1_5_variables;
+
+/**
+ *
+ * @author garfi
+ */
+public class EVA1_5_VARIABLES {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       String Nombre;
+       byte Edad;
+       //INICIALIZACIÓN// = OPERADOR DE ASIGNACIÓN
+       Nombre="Allison Garfio";
+       Edad=17;
+       //USO DE VARIABLES
+       System.out.println(Nombre);
+       System.out.println(Edad);
+    }
+    
+}
